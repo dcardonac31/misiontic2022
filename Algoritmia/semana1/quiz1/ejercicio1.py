ab9377619a346daa8303093e6c57787ad7d00ec1
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2021-05-22 10:49:37 -0500

    Semana 1

---
+++ Algoritmia/semana1/quiz1/ejercicio1.py
@@ -0,0 +1,121 @@
+#Ejercicio 1
+a = 8                                  
+x = 20.       
+b = 6                                
+y = 3.53
+c = 9                                
+z = 15
+d = 7                                
+w = 1.72
+resultado_1 = z / x + b * w * (c - b) / a
+print("resultado 1 :",{resultado_1})
+#Ejercicio 2
+a = 8                                  
+x = 20.        
+b = 6                                
+y = 3.53
+c = 9                                
+z = 15
+d = 7                                
+w = 1.72
+resultado_2 = a / (b + c // (d + 1) * (a + b) - a) ** b ** a + c 
+print("resultado 2 :",{resultado_2})
+#Ejercicio 3
+a = 3                                  
+x = 2. 
+b = 5                                
+y = 3.5
+c = 2                                
+z = 5.
+d = 4                                
+w = 1.2
+resultado_3 = z/x+b*w*(c-b)/a 
+print("resultado 3 :",{resultado_3})
+#Ejercicio 4
+a = 8                                  
+x = 20.         
+b = 6                               
+y = 3.53
+c = 9                                
+z = 15.
+d = 7                                
+W = 1.72
+resultado_4 = a * b / 2 + 1 
+print("resultado 4 :",{resultado_4})
+#Ejercicio 5
+a = 3                                  
+x = 2. 
+b = 5                                
+y = 3.5
+c = 2                                
+z = 5.
+d = 4                                
+w = 1.2
+resultado_5 = x / (b + c / (y + 1) * (a + b) - a) ** b ** a + w
+print("resultado 5 :",{resultado_5})
+#Ejercicio 8
+a = 3                                  
+x = 2. 
+b = 5                                
+y = 3.5
+c = 2                                
+z = 5.
+d = 4                                
+w = 1.2
+resultado_8 = a + (b - c * (d - (a + b) * (c - c / d * a) % a + b * c) - (b ** c)) ** b
+print("resultado 8 :",{resultado_8})
+#Ejercicio 10
+a = 8                                  
+x = 20.         
+b = 6                                
+y = 3.53
+c = 9                                
+z = 15.
+d = 7                                
+w = 1.72
+resultado_10 = c / x ** a
+print("resultado 10 :",{resultado_10})
+#Ejercicio 11
+a = 8                                  
+x = 20.         
+b = 6                                
+y = 3.53
+c = 9                               
+z = 15.
+d = 7                                
+w = 1.72
+resultado_11 = a + (b - c * (d - (a + b) * (c - c / d * a) % a + b * c) - (b ** c)) ** b
+print("resultado 11 :",{resultado_11})
+#Ejercicio 13
+a = 3
+x = 2. 
+b = 5                                
+y = 3.5
+c = 2
+z = 5.
+d = 4                                
+w = 1.2
+resultado_13 = a / (b + c // (d + 1) * (a + b) - a) ** b ** a + c
+print("resultado 13 :",{resultado_13})
+#Ejercicio 16
+a = 3                                  
+x = 2. 
+b = 5                                
+y = 3.5
+c = 2                                
+z = 5.
+d = 4                                
+w = 1.2
+resultado_16 = c / x ** a
+print("resultado 16 :",{resultado_16})
+#Ejercicio 17
+a = 3                                
+x = 2. 
+b = 5                                
+y = 3.5
+c = 2                                
+z = 5.
+d = 4                                
+w = 1.2
+resultado_17 = a * b / 2 + 1
+print("resultado 17 :",{resultado_17})
---
+++ Algoritmia/semana1/quiz1/quiz1.py
@@ -0,0 +1,19 @@
+# 1
+# (a-((b+c)/(d-e))+e-f**2.3)/(4.5+d)
+
+# 2
+# La expresión algebraica correspondiente a la siguiente expresión de computador
+#a – s ** (b / c) * 5.84 + d // e // f  es
+
+from typing import Match
+
+
+variable = 1
+
+print(type(variable))
+
+
+
+
+
+
